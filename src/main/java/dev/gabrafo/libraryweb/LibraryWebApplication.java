6f0fd334265550051812947ec3e5e4086214ccc4
Gabrafo <gafagundesmesquita@gmail.com>
2024-12-20 17:07:31 -0300

    layer package rebase over

--- src/main/java/dev/gabrafo/libraryweb/LibraryWebApplication.java
+++ src/main/java/dev/gabrafo/libraryweb/LibraryWebApplication.java
@@ -2,8 +2,10 @@ package dev.gabrafo.libraryweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LibraryWebApplication {
 
 	public static void main(String[] args) {
